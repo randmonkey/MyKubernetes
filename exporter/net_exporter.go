dce720da527bd55b3ed86c41dd645bced6be476c
01deyishu <01deyishu@gmail.com>
2018-01-23 19:12:56 +0800

    获取bond列表和速率

--- exporter/net_exporter.go
+++ exporter/net_exporter.go
@@ -7,27 +7,32 @@ import (
 	"github.com/vishvananda/netlink"
 	"io/ioutil"
 	"path"
+	"strings"
 )
 
-//var data string = `node_bonding_active{master="bond10"} 1`
+// var data string = `node_bonding_active{master="bond10"} 1`
 
-func getNicStatus() {
-	//var ifnames []string
+func getBondList() (ifname []string) {
 	links, err := netlink.LinkList()
 	if err != nil {
-		fmt.Errorf("Get link list err :", err.Error())
+		fmt.Errorf("Get link list err:", err.Error())
 	}
 	for _, link := range links {
 		if link.Type() == "bond" {
-			ifname := link.Attrs().Name
-			filename := path.Join("/sys/class/net/", ifname, "speed")
-			buf, err := ioutil.ReadFile(filename)
-			if err != nil {
-				fmt.Errorf("Error open bond speed file", err.Error())
-			}
-			fmt.Println(ifname, string(buf))
+			ifname = append(ifname, link.Attrs().Name)
 		}
 	}
+	return
+}
+
+func getBondSpeed(bond string) (speed string) {
+	filename := path.Join("/sys/class/net/", bond, "speed")
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Errorf("Error open bond speed file", err.Error())
+	}
+	speed = strings.Replace(string(buf), "\n", "", -1)
+	return
 }
 
 // func handler(w http.ResponseWriter, r *http.Request) {
@@ -37,5 +42,11 @@ func getNicStatus() {
 func main() {
 	// http.HandleFunc("/", handler)
 	// http.ListenAndServe("0.0.0.0:9001", nil)
-	getNicStatus()
+	bondList := getBondList()
+	//fmt.Println(len(bondList))
+
+	for _, bond := range bondList {
+		speed := getBondSpeed(bond)
+		fmt.Println(bond, speed)
+	}
 }
