7cb89c4af646747f9206756c90a974510c73f8e9
01deyishu <01deyishu@gmail.com>
2018-07-29 17:18:48 +0800

    新版ingress-log-exporter

--- exporter/ingress-log-exporter.go
+++ exporter/ingress-log-exporter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -11,7 +12,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/hpcloud/tail"
+	"github.com/fsnotify/fsnotify"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -36,6 +37,13 @@ var (
 		prometheus.CounterOpts{
 			Name: "status0xxCounter",
 			Help: "domainname 0xx counter",
+		},
+		[]string{"host"},
+	)
+	oneCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "status1xxCounter",
+			Help: "domainname 1xx counter",
 		},
 		[]string{"host"},
 	)
@@ -76,10 +84,13 @@ var (
 	)
 )
 
+var PATH string = "/var/log/containers/"
+
 func init() {
 	prometheus.MustRegister(sendSizeCounter)
 	prometheus.MustRegister(receiveSizeCounter)
 	prometheus.MustRegister(zeroCounter)
+	prometheus.MustRegister(oneCounter)
 	prometheus.MustRegister(twoCounter)
 	prometheus.MustRegister(threeCounter)
 	prometheus.MustRegister(fourCounter)
@@ -105,6 +116,7 @@ type savefile struct {
 
 type counters struct {
 	Zero            float64 `json:"zero"`
+	One             float64 `json:"one"`
 	Tow             float64 `json:"tow"`
 	Three           float64 `json:"three"`
 	Four            float64 `json:"four"`
@@ -112,46 +124,6 @@ type counters struct {
 	Six             float64 `json:"six"`
 	Send_counter    float64 `json:"send_counter"`
 	Receive_counter float64 `json:"receive_counter"`
-}
-
-func getFile() (filepath string) {
-	var filename string
-	var path string = "/var/log/containers/"
-	dir, err := ioutil.ReadDir(path)
-	if err != nil {
-		fmt.Println("error of read log directory")
-	}
-	for _, v := range dir {
-		if j, _ := regexp.MatchString("(.*)-ingress-controller(.*)log", v.Name()); j {
-			filename = v.Name()
-		}
-	}
-	filepath = path + filename
-	return
-}
-
-func tailLog(ch chan log) {
-	filepath := getFile()
-	t, err := tail.TailFile(filepath, tail.Config{Follow: true, ReOpen: true})
-	if err != nil {
-		fmt.Println("error of tail access.log!", err.Error())
-	}
-	for line := range t.Lines {
-		var ls logstr
-		var logs log
-		err := json.Unmarshal([]byte(line.Text), &ls)
-		if err != nil {
-			fmt.Println("error of unmarshal line.text -> logstr", err.Error())
-		}
-		ls.Log = strings.Replace(ls.Log, "\\", "", -1)
-		if isok, _ := regexp.MatchString("^[WIE]?([0-9]+)", ls.Log); !isok {
-			err := json.Unmarshal([]byte(ls.Log), &logs)
-			if err != nil {
-				fmt.Println("error of unmarshal logstr -> log", err.Error())
-			}
-			ch <- logs
-		}
-	}
 }
 
 func addSaveDate(filepath string) {
@@ -165,33 +137,37 @@ func addSaveDate(filepath string) {
 		sendSizeCounter.With(prometheus.Labels{"host": k}).Add(v.Send_counter)
 		receiveSizeCounter.With(prometheus.Labels{"host": k}).Add(v.Receive_counter)
 		zeroCounter.With(prometheus.Labels{"host": k}).Add(v.Zero)
+		oneCounter.With(prometheus.Labels{"host": k}).Add(v.One)
 		twoCounter.With(prometheus.Labels{"host": k}).Add(v.Tow)
 		threeCounter.With(prometheus.Labels{"host": k}).Add(v.Three)
 		fourCounter.With(prometheus.Labels{"host": k}).Add(v.Four)
 		fiveCounter.With(prometheus.Labels{"host": k}).Add(v.Five)
 		sixCounter.With(prometheus.Labels{"host": k}).Add(v.Six)
 	}
+	fmt.Println("add saved date success")
 }
 
 func counter(ch chan log) {
-	var path string = "/var/log/containers/"
 	var file string = "ingress-log-counter.json"
-	filepath := path + file
+	filepath := PATH + file
 	var metrics map[string]*counters
 	metrics = make(map[string]*counters)
 	if _, err := os.Stat(filepath); !os.IsNotExist(err) {
 		addSaveDate(filepath)
 	}
-	ticker := time.NewTicker(time.Second * 5)
+
 	go func() {
+		ticker := time.NewTicker(time.Second * 5)
 		for _ = range ticker.C {
 			save, _ := json.Marshal(metrics)
 			err := ioutil.WriteFile(filepath, save, 0664)
 			if err != nil {
-				fmt.Println("err or write save file", err.Error())
+				fmt.Println("err of write save file", err.Error())
 			}
+			fmt.Println("success of save current date to fail")
 		}
 	}()
+
 	for l := range ch {
 		if metrics[l.Host] == nil {
 			var p *counters = new(counters)
@@ -208,6 +184,9 @@ func counter(ch chan log) {
 		case "0":
 			zeroCounter.With(prometheus.Labels{"host": l.Host}).Add(1)
 			metrics[l.Host].Zero++
+		case "1":
+			oneCounter.With(prometheus.Labels{"host": l.Host}).Add(1)
+			metrics[l.Host].One++
 		case "2":
 			twoCounter.With(prometheus.Labels{"host": l.Host}).Add(1)
 			metrics[l.Host].Tow++
@@ -229,9 +208,103 @@ func counter(ch chan log) {
 	}
 }
 
+func findNewFile() (newFile string) {
+	var filename string
+	dir, err := ioutil.ReadDir(PATH)
+	if err != nil {
+		fmt.Println("error of read log directory")
+	}
+	for _, v := range dir {
+		if j, _ := regexp.MatchString("(.*)-ingress-controller(.*)log", v.Name()); j {
+			filename = v.Name()
+		}
+	}
+	newFile = PATH + filename
+	return
+}
+
+func watchDir(ch chan log) {
+	dir := "/var/log/containers/"
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("error of create new watcher", err.Error())
+	}
+	defer w.Close()
+
+	err = w.Add(dir)
+	if err != nil {
+		fmt.Println("error of add dir watcher", err.Error())
+	}
+
+	f := findNewFile()
+	go tailf(ch, f)
+
+	for {
+		event := <-w.Events
+		if event.Op&fsnotify.Create == fsnotify.Create {
+			if isok, _ := regexp.MatchString("(.*)-ingress-controller(.*)log", event.Name); isok {
+				go tailf(ch, event.Name)
+			}
+		}
+	}
+}
+
+func tailf(ch chan log, filename string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("error of create new watcher")
+	}
+	defer watcher.Close()
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("error of open file:", err.Error())
+	}
+	file.Seek(0, 2)
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	err = watcher.Add(filename)
+	if err != nil {
+		fmt.Println("Tailf: error of add watcher", err.Error())
+	}
+	fmt.Println("Tailf: Starting tail file:", filename)
+
+	for {
+		event := <-watcher.Events
+		if event.Op&fsnotify.Write == fsnotify.Write {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Tailf: error of read line from reader", err.Error())
+				return
+			}
+
+			var ls logstr
+			var logs log
+			err = json.Unmarshal([]byte(line), &ls)
+			if err != nil {
+				fmt.Println("error of unmarshal line.text -> logstr", err.Error())
+			}
+			ls.Log = strings.Replace(ls.Log, "\\", "", -1)
+			if isok, _ := regexp.MatchString("^[WIE]?([0-9]+)", ls.Log); !isok {
+				err := json.Unmarshal([]byte(ls.Log), &logs)
+				if err != nil {
+					fmt.Println("error of unmarshal logstr -> log", err.Error())
+				}
+				ch <- logs
+			}
+		} else {
+			fmt.Println("Tailf: log file change", event.Op, event.Name)
+			return
+		}
+
+	}
+}
+
 func main() {
 	ch := make(chan log, 1000)
-	go tailLog(ch)
+	go watchDir(ch)
 	go counter(ch)
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8888", nil)
