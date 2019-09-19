86a2ae8e7835ff04c425085c5fea70482ef547e6
yanshen <yanshen@qiniu.com>
2019-09-19 16:11:44 +0800

    优化quagga的兼容性，支持可以通过configmap挂载ospfd.conf

--- HA/quagga/change_quagga_config.sh
+++ HA/quagga/change_quagga_config.sh
@@ -11,5 +11,13 @@ _OSPFD_IP_=$_ROUTER_ID_
 sed -i "s@_OSPFD_IP_@$_OSPFD_IP_@g" /etc/quagga/debian.conf
 sed -i "s@_NIC_NAME_@$_NIC_NAME_@g;s@_ROUTER_ID_@$_ROUTER_ID_@g;s@_GATEWAY_@$_GATEWAY_@g;s@_NEIGHBOR_@ $_NEIGHBOR_@g" /etc/quagga/ospfd.conf
 
-service quagga start
+FILE=/etc/quagga/oppf-cfg/ospfd.conf
+if test -f "$FILE"; then
+    service quagga start
+else
+    /usr/lib/quagga/zebra --daemon -A 127.0.0.1
+    /usr/lib/quagga/ospfd --daemon -f /etc/quagga/ospf-cfg/ospfd.conf -A 0.0.0.0
+    /usr/lib/quagga/watchquagga --daemon zebra ospfd
+fi
+
 tail -f /dev/null
