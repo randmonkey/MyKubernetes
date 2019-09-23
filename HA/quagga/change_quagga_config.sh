6791b2a3b797137033111de8872c39cd935dbc50
yitao <richardyi0110@hotmail.com>
2019-09-23 14:44:35 +0800

    update quagga.yaml

--- HA/quagga/change_quagga_config.sh
+++ HA/quagga/change_quagga_config.sh
@@ -6,10 +6,21 @@ if [[ ${_NIC_NAME_} == "" ]]; then
 fi
 if [[ ${_GATEWAY_} == "" ]]; then
 	echo "environment variable _GATEWAY_ not found, configure ospf by interface ${_NIC_NAME_}..."
-	_GATEWAY_=`ip route | grep default | awk '{print $3}'`
+	_GATEWAY_=`ip route | grep via | grep ${_NIC_NAME_} | head -n 1 | awk '{print $3}'`
+	echo "gateway is ${_GATEWAY_}"
+fi
+
+if [[ ${_NEIGHBOR_} == "" ]]; then
+	echo "environment variable _NEIGHBOR_ not found, find neighbor network from gateway ${_GATEWAY_}"
 	_GATEWAY_CIDR_=(`echo $_GATEWAY_ | awk -F "." '{print $1"."$2"."$3}'`)
-	_ROUTER_ID_=`ip addr show $_NIC_NAME_ | grep $_GATEWAY_CIDR_ | awk '{print $2}' | awk -F "/" '{print $1}'`
-	_NEIGHBOR_=`ip route | grep kernel  | grep $_GATEWAY_CIDR_ | awk '{print $1}'`
+	_NEIGHBOR_=`ip route | grep kernel  | grep $_GATEWAY_CIDR_ | head -n 1 | awk '{print $1}'`
+	echo "neighbor network is ${_NEIGHBOR_}"
+fi
+
+if [[ ${_ROUTER_ID_} == "" ]]; then
+	echo "environment variable _ROUTER_ID_ not found, use the first IP of ${_NIC_NAME_} as router ID"
+	_ROUTER_ID_=`ip addr show $_NIC_NAME_ | grep inet | head -n 1 | awk '{print $2}' | awk -F "/" '{print $1}'`
+	echo "router ID is ${_ROUTER_ID_}"
 fi
 #debian.conf
 _OSPFD_IP_=$_ROUTER_ID_
